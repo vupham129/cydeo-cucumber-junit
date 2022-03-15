4014fabe1213aefc86d608a3dbc890837c00a6fa
vupha <vupham129@gmail.com>
2022-03-15 16:59:35 -0700

    changed

--- src/test/java/com/cydeo/runners/CukesRunner.java
+++ src/test/java/com/cydeo/runners/CukesRunner.java
@@ -6,9 +6,11 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
+        plugin = "html:target/cucumber-reports.html",
         features = "src/test/resources/features",
         glue = "com/cydeo/step_definitions",
-        dryRun = false
+        dryRun = false,
+        tags =""
 )
 public class CukesRunner {
 }
